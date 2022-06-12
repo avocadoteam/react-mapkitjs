5e9b9d40bb9b855d85d1c52eb9bd8973ec897c65
woophi <mik.kon6610@gmail.com>
2022-06-12 14:47:21 +0200

    try another solution

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-export { AppleMaps } from './NAppleMaps';
-export { AppleMapContext } from './mapContext';
-export { Annotation } from './Annotation';
-export { ImageAnnotation } from './ImageAnnotation';
-export { CurrentLocationOverride } from './CurrentLocationOverride';
-
-// Enums
-export { MapType, ColorScheme, FeatureVisibility } from './enums';
+export * from './MapkitProvider'
+export * from './Map'
+export * from './Marker'
+export * from './useMap'
